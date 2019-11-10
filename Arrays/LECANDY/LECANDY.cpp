e884448654c323070748863ff70d107fad81d325
pritamlad <pritamlad18@gmail.com>
2019-11-10 21:32:02 +0530

    environment updated

--- Arrays/LECANDY/LECANDY.cpp
+++ Arrays/LECANDY/LECANDY.cpp
@@ -44,8 +44,15 @@ Case 2. Even if we give four candies to the first elephant and two candies to th
 #include <iostream>
 using namespace std;
 
+
 int main()
-{
+{	
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif  
+
+
 	long long int t ;
 	cin >> t;
 	while(t--)
@@ -69,4 +76,4 @@ int main()
 			cout << "NO" << endl;
 		}
 	}
-}
+} 
